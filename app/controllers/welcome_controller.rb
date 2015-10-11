9ea0d7f1f027221f8633a05b9b077e23ecbdca23
Anna <mafackahero@gmail.com>
2015-10-11 22:17:36 +0300

    Add Populator

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,8 +9,8 @@ class WelcomeController < ApplicationController
       _tags_count = ContentView.any_tags(tag.id).count
       _class =
         case _tags_count
-        when 0..1 then 's'
-        when 1..2 then 'm'
+        when 0..15 then 's'
+        when 15..25 then 'm'
         else 'l'
         end
       @articles << { tag: tag, css_class: _class }
