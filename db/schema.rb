24d13d6d02ee5c52ecf94fd2fc13b421b1585f5d
Anna <mafackahero@gmail.com>
2015-10-12 00:06:38 +0300

    Prepere for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -38,26 +38,6 @@ ActiveRecord::Schema.define(version: 20151010022756) do
 
   add_index "comments", ["target_id", "target_type"], name: "index_comments_on_target_id_and_target_type", using: :btree
 
-  create_table "dm_models_headers", force: true do |t|
-    t.string  "name",      limit: 50
-    t.string  "value",     limit: 50
-    t.integer "report_id"
-  end
-
-  create_table "dm_models_links", force: true do |t|
-    t.string  "name",      limit: 50
-    t.string  "href",      limit: 50
-    t.string  "rel",       limit: 50
-    t.string  "target",    limit: 50
-    t.integer "report_id"
-  end
-
-  create_table "dm_models_reports", force: true do |t|
-    t.string   "url",  limit: 50
-    t.datetime "time"
-    t.string   "ip",   limit: 39
-  end
-
   create_table "events", force: true do |t|
     t.string   "title"
     t.text     "address"
@@ -67,22 +47,6 @@ ActiveRecord::Schema.define(version: 20151010022756) do
   end
 
   add_index "events", ["tag_ids"], name: "index_events_on_tag_ids", using: :gin
-
-  create_table "headers", id: false, force: true do |t|
-    t.integer "id",        default: "nextval('headers_id_seq'::regclass)", null: false
-    t.text    "key"
-    t.text    "value"
-    t.integer "report_id"
-  end
-
-  create_table "links", id: false, force: true do |t|
-    t.integer "id",                   default: "nextval('links_id_seq'::regclass)", null: false
-    t.text    "name"
-    t.text    "href"
-    t.text    "rel"
-    t.string  "target",    limit: 15
-    t.integer "report_id"
-  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -94,25 +58,11 @@ ActiveRecord::Schema.define(version: 20151010022756) do
 
   add_index "posts", ["tag_ids"], name: "index_posts_on_tag_ids", using: :gin
 
-  create_table "reports", id: false, force: true do |t|
-    t.integer "id",   default: "nextval('reports_id_seq'::regclass)", null: false
-    t.text    "url"
-    t.text    "time"
-    t.text    "ip"
-  end
-
   create_table "tags", force: true do |t|
     t.string   "name"
     t.string   "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "videos", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
 end
