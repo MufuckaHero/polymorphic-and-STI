118b9a15751cf7c1032c3647c08a8bc0376beabe
Anna <mafackahero@gmail.com>
2015-10-15 06:58:54 +0300

    Hstore/Fragment Caching

---
+++ db/migrate/20151014183319_add_hstore_to_posts.rb
@@ -0,0 +1,13 @@
+class AddHstoreToPosts < ActiveRecord::Migration
+  def up
+    execute 'CREATE EXTENSION IF NOT EXISTS hstore;'
+    
+    add_column :posts, :settings, :hstore, default: '', null: false
+  end
+
+  def down
+    execute 'DROP EXTENSION hstore;'
+
+    add_column :posts, :settings
+  end
+end
